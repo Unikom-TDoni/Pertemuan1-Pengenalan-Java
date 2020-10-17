2a2857e33648f039fc0fe0934f0d20710e11935b
lncodes <lastnightcodes@gmail.com>
2020-10-17 19:48:57 +0700

    Squashed 'PBO10K-10119907-Latihan8-Pecahan/' content from commit 3191167
    git-subtree-dir: PBO10K-10119907-Latihan8-Pecahan
    git-subtree-split: 319116743f01ba651699379b6ed72f6f701f90f1

---
+++ src/main/java/PBO10K/NIM10119907/Latihan8/Pecahan/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan8.Pecahan;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk belajar pecahan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
