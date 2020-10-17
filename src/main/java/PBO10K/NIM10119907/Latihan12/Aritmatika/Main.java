6ba199cae1384a6a32a60e9cde69e7934e63d396
lncodes <lastnightcodes@gmail.com>
2020-10-17 19:50:10 +0700

    Squashed 'PBO10K-10119907-Latihan12-Aritmatika/' content from commit ecc98a1
    git-subtree-dir: PBO10K-10119907-Latihan12-Aritmatika
    git-subtree-split: ecc98a12565093517e457e7da50fca4b4bef27e8

---
+++ src/main/java/PBO10K/NIM10119907/Latihan12/Aritmatika/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan12.Aritmatika;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk belajar aritmatika
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+
+
+
+
+    }
+    
+}
