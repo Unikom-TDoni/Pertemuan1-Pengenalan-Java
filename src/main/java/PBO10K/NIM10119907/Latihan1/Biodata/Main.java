287c8e490d0f5e4fe72dd138ed166c7256b3bff5
lncodes <lastnightcodes@gmail.com>
2020-10-17 19:45:37 +0700

    Squashed 'PBO10K-10119907-Latihan1-Biodata/' content from commit 47f48f9
    git-subtree-dir: PBO10K-10119907-Latihan1-Biodata
    git-subtree-split: 47f48f9488868ad184cad53ec750ad046c71036c

---
+++ src/main/java/PBO10K/NIM10119907/Latihan1/Biodata/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan1.Biodata;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk menampilkan biodata
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: T Doni Indraprasta P");
+        System.out.println("Alamat\t\t: Bandung Inten Indah no 15");
+        System.out.println("TEMPAT LAHIR\t\t: Yogyakarta");
+        System.out.println("TANGGAL LAHIR\t\t: 1 Juli 1999");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Main Musik");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Fanta");
+    }
+    
+}
