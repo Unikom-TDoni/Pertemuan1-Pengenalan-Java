90943197a57293305fe5005ef755be6c05a6ae19
lncodes <lastnightcodes@gmail.com>
2020-10-17 13:01:09 +0700

    Initial Commit

---
+++ src/main/java/PBO10K/NIM10119907/Latihan10/Logika/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan10.Logika;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk belajar logika
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = umur >= 17;
+        System.out.println("Status dewasa : " + dewasa);
+    }    
+}
