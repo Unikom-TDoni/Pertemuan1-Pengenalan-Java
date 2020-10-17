29e3879a38488745f120b13728394d87a9c68eb5
lncodes <lastnightcodes@gmail.com>
2020-10-17 19:50:25 +0700

    Squashed 'PBO10K-10119907-Latihan13-Relational/' content from commit 05549a9
    git-subtree-dir: PBO10K-10119907-Latihan13-Relational
    git-subtree-split: 05549a9ecde749011ac5cb4293100765e9121c96

---
+++ src/main/java/PBO10K/NIM10119907/Latihan13/Relational/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119907.Latihan13.Relational;
+
+/**
+ *
+ * @author
+ * Nama     : T Doni
+ * Kelas    : IF10K
+ * NIM      : 10119907
+ * Deskripsi Program : Program ini digunakan untuk belajar relational
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b = " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("b >= a = " + (bil2 >= bill));
+        System.out.println("b <= a = " + (bil2 <= bill));
+
+
+    }
+    
+}
